862a07f7bb5da6caa29f75e7f404643334f19b9a
sctveb <sctveb@gmail.com>
2019-09-15 15:48:04 +0900

    2019-05-15-02 change port

--- server.js
+++ server.js
@@ -7,14 +7,13 @@ const dotenv = require('dotenv');
 
 const dev = process.env.NODE_ENV !== 'production';
 const prod = process.env.NODE_ENV === 'production';
-
+const port =  process.env.PORT || 8081;
 const app = next({ dev });
 const handle = app.getRequestHandler();
 dotenv.config();
 
 app.prepare().then(() => {
   const server = express();
-
   server.use(morgan('dev'));
   server.use(express.json());
   server.use(express.urlencoded({ extended: true }));
@@ -41,7 +40,7 @@ app.prepare().then(() => {
     return handle(req, res);
   });
 
-  server.listen(3060, () => {
+  server.listen(PORT, () => {
     console.log('next+express running on port 3060');
   });
 });
